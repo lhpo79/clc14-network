b99bef7497c839a6510fb04d752a9833016b3e05
EC2 Default User <ec2-user@ip-172-31-38-242.ec2.internal>
2025-11-25 22:59:36 +0000

    Ajustando configuracoes

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "clc14-luiz-terraform"
+    bucket = "luiz-terraform-clc14"
     key    = "network/terraform2.tfstate"
     region = "us-east-1"
     use_lockfile = true
